8ed61dd41acd21cfdd13a008cbe5adab26239858
Nishant Srivastava <nishant@dataglen.com>
2017-08-26 21:14:48 +0530

    making reference id unique

--- alerting_system/alerts/models.py
+++ alerting_system/alerts/models.py
@@ -10,7 +10,7 @@ class AlertManager(models.Model):
 
 
 class Alerts(models.Model):
-    reference_id=models.CharField(max_length=100, null=False, blank=False)
+    reference_id=models.CharField(max_length=100, null=False, blank=False, unique=True)
     delay = models.IntegerField(null=False, blank=False)
     description = models.CharField(max_length=200, null=False, blank=False)
     active = models.BooleanField(null=False, blank=False, default=True)
